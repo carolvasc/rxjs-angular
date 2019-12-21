d981c720cce47648af7c062ada974480e19c20c3
Carol <cvsouza04@gmail.com>
2019-12-21 01:59:51 -0300

    Basic Subject implementation

--- src/app/subject/subject.component.ts
+++ src/app/subject/subject.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Subject, from } from 'rxjs';
 
 @Component({
   selector: 'app-subject',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubjectComponent implements OnInit {
 
+  subject = new Subject<number>();
+
   constructor() { }
 
   ngOnInit() {
+    this.firstExample();
+    this.secondExample();
+  }
+
+  createObservable(name: String) {
+    this.subject.subscribe({
+      next: (v) => this.addItem(`${name}: ${v}`)
+    });
+  }
+
+  firstExample() {
+    this.createObservable('observerA');
+    this.createObservable('observerB');
+
+    this.subject.next(1);
+    this.subject.next(2);
+  }
+
+  secondExample() {
+    this.createObservable('observerA');
+    this.createObservable('observerB');
+
+    const observable = from([1, 2, 3]);
+
+    observable.subscribe(this.subject);
   }
 
 
